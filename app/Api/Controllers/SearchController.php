b0b0fa4fdfa04363f3d93bac5c6b99589e95aa4e
Eric Haberstroh <gpg@erixpage.de>
2016-11-12 14:56:23 +0100

    Change twitter database structure, implement search

--- app/Api/Controllers/SearchController.php
+++ app/Api/Controllers/SearchController.php
@@ -12,6 +12,30 @@ class SearchController extends BaseController
             return $this->response->errorBadRequest();
         }
 
-        $q = $request->input('q');
+        $search = '%'.str_replace(' ', '%', $request->input('q')).'%';
+
+        $persons = Person::where('firstname', 'LIKE', $search)
+        				 ->orWhere('lastname', 'LIKE', $search)
+        				 ->orWhere('peerage', 'LIKE', $search)
+        				 ->orWhere('title', 'LIKE', $search)
+        				 ->get();
+
+        $factions = Faction::where('name', 'LIKE', $search)->get();
+
+        $data = [];
+
+        foreach ($factions as $faction) {
+        	$data[] = [
+        		'url' => '/#/faction/'.$faction->id,
+        		'caption' => 'Fraktion '.$faction->name
+        	];
+        }
+
+        foreach ($persons as $person) {
+        	$data[] = [
+        		'url' => '/#/person/'.$person->id,
+        		'caption' => $person->firstname.' '.trim($person->peerage.' '.$person->lastname).' ('.$person->faction->name.')'
+        	];
+        }
     }
 }
